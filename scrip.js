329d83fcc14e3e2fad6de8d704207a2311e33909
Dragoş <edragos33@gmail.com>
2025-06-01 15:01:12 +0200

    basic scene added

--- scrip.js
+++ scrip.js
@@ -1,1 +1,30 @@
-console.log("test");
+import * as THREE from 'three';
+
+// ------ Scene ------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 10);
+
+// ------ Objects ------
+
+
+// ------ Renderer ------
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
+
+
+// ------ Responsive page ------
+window.addEventListener('resize', () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+});
+
+renderer.render(scene, camera);
