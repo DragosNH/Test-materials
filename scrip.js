631ba0f9b482a4f3b339cd733204f857284014bb
Dragoş <edragos33@gmail.com>
2025-06-01 15:14:25 +0200

    multicolor cube test

--- scrip.js
+++ scrip.js
@@ -1,12 +1,10 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // ------ Scene ------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 10);
-
-// ------ Objects ------
-
 
 // ------ Renderer ------
 const renderer = new THREE.WebGLRenderer();
@@ -14,6 +12,20 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls( camera, renderer.domElement );
+
+// ------ Objects ------
+const geometry = new THREE.BoxGeometry(5,5,5);
+const materials = [
+  new THREE.MeshBasicMaterial({color: 0xff0000}),
+  new THREE.MeshBasicMaterial({color: 0x0000ff}),
+  new THREE.MeshBasicMaterial({color: 0x00ff00}),
+  new THREE.MeshBasicMaterial({color: 0xff00ff}),
+  new THREE.MeshBasicMaterial({color: 0x00ffff}),
+  new THREE.MeshBasicMaterial({color: 0xffff00})
+];
+const cube = new THREE.Mesh(geometry, materials);
+scene.add(cube);
 
 
 // ------ Responsive page ------
@@ -27,4 +39,12 @@ window.addEventListener('resize', () => {
     renderer.setSize(width, height);
 });
 
-renderer.render(scene, camera);
+
+const animate = () => {
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+animate();
